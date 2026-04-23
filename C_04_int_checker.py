3c51670ff0ae02d6d5b04236383fcbb48ee1c2ca
Benjaphorn1 <166575282+Benjaphorn1@users.noreply.github.com>
2026-04-24 09:02:53 +1200

    Working integer checker function

---
+++ C_04_int_checker.py
@@ -0,0 +1,43 @@
+def num_check(question, num_type="float", exit_code=None):
+    """Checks that response is a float / integer more than zero"""
+
+    if num_type == "float":
+        error = "Please enter a number more than 0."
+    else:
+        error = "Please enter an integer more than 0."
+
+    while True:
+
+        response = input(question)
+
+        # check for exit code and return it if entered
+        if response == exit_code:
+            return response
+
+        # check datatype is correct and that number
+        # is more than zero
+        try:
+
+            if num_type == "float":
+                response = float(response)
+            else:
+                response = int(response)
+
+            if response > 0:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# set up loop for testing
+while True:
+    number = num_check("Quantity being made: ", "float","xxx")
+    print(number)
+
+    # if exit code is entered break
+    if number =="xxx":
+        break
+
